47398fd424537ab6c20410a04eddc5110e7490a8
Konstantin <Steelmen87@bk.ru>
2021-03-11 11:19:49 +0300

    lesson14

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+      <img src='https://photos.gurushots.com/unsafe/0x500/9bc838510e2340ef6bc045d1ad9316f7/3_28af67370b9a2bdb8ae26e2a7a006756.jpg'/>
+      </div>
+      <div>
+        avatar + discription
+      </div>
+      <div>
+        My post
+        <div>
+          New Post
+        </div>
+        <div>
+          <div>
+          post 1
+          </div>
+          <div>
+          post 2
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+}
+
+export default Profile;
