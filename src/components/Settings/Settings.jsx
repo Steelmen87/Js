43367dae3a154c96b6ad0d9b4dcf5f06216bce2d
Konstantin <Steelmen87@bk.ru>
2021-03-15 22:36:48 +0300

    19

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import s from './New.module.css';
+
+const New = (props) => {
+    return (
+        <div>
+            New
+        </div>
+    );
+}
+export default New;
